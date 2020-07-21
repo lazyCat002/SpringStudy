14c79b30c1bc164048ee165fa170523a14e0c267
杨思藏 <sicang.yang@k7.cn>
2020-07-21 11:54:58 +0800

    feat: 初识SpringBoot(HelloWorld)

---
+++ untitled2/src/main/java/com/bee/sample/ysc/YscApplication.java
@@ -0,0 +1,11 @@
+package com.bee.sample.ysc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YscApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YscApplication.class,args);
+    }
+}
